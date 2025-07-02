12871a2854812996432d06102c32aac8ddeb122a
Deni Andreawan <deniandrwan@gmail.com>
2025-07-02 13:20:54 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
-            Copyright &copy; 2024. Dias Atmaja
+            Copyright &copy; 2024. Sekolah AI
           </p>
         </div>
       </div>
